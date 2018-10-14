8b5da5a1ac34293323eccfef1daa0c1db0d11551
ConfinedFish <alexzeigler0317@gmail.com>
2018-10-14 13:12:33 -0700

    Created project

---
+++ src/project2/Sequence.java
@@ -0,0 +1,15 @@
+package project2;
+
+public class Sequence<E> {
+    private int size;
+    private E[] data;
+    
+    
+    
+    public int size(){
+        return size;
+    }
+    public void append(E element){
+        
+    }
+}
